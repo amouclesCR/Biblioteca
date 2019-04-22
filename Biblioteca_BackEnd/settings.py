7ccb8a1ec4fc2bd30cca1bc6dc736c128d449b04
amoucles <alexcuatroojos@gmail.com>
2019-04-22 16:48:15 -0600

    coneccion a base de datos

--- Biblioteca_BackEnd/settings.py
+++ Biblioteca_BackEnd/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'Biblioteca_BackEnd.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'biblioteca',
+            'USER': 'root',
+            'PASSWORD': 'AMBU_DB123',
+            'HOST': 'localhost',
+            'PORT': '3306',
     }
 }
 
