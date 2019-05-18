eb4f3945aefb16bd7638ac3429adee24cdc86a32
amoucles <alexcuatroojos@gmail.com>
2019-05-17 21:58:12 -0600

    ajuste de api de scanner

--- Biblioteca_BackEnd/api/viewsFolder/activoView.py
+++ Biblioteca_BackEnd/api/viewsFolder/activoView.py
@@ -59,9 +59,9 @@ class ActivoByNumeroActivoLView(generics.ListAPIView):
    
     def get(self, request, *args, **kwargs):
         numeroActivo = kwargs.get('numeroactivo') 
-        print("tas")
-        query = AMBU_Activo.objects.filter(act_numero_activo=numeroActivo)
 
-        serializer = activoSerializer(query, many=True)
+        query = AMBU_Activo.objects.get(act_numero_activo=numeroActivo)
+
+        serializer = activoSerializer(query, many=False)
 
         return Response(serializer.data)
