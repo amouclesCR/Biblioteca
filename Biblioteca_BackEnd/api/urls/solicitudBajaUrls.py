ee8e06cf9c9f9b70a034bc7278310f907a672e89
amoucles <alexcuatroojos@gmail.com>
2019-06-07 13:26:47 -0600

    ajuste de url de solicitud

--- Biblioteca_BackEnd/api/urls/solicitudBajaUrls.py
+++ Biblioteca_BackEnd/api/urls/solicitudBajaUrls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from Biblioteca_BackEnd.api.viewsFolder.solicitudBajaView import solicitudBajaLCView, solicitudBajaRUView, solicitudByUsuarioL, solicitudAprobar, solicitudBajaLView
+from Biblioteca_BackEnd.api.viewsFolder.solicitudBajaView import solicitudRechazar, solicitudBajaLCView, solicitudBajaRUView, solicitudByUsuarioL, solicitudAprobar, solicitudBajaLView
 app_name = "api"
 SOLICITUD = 'solicitud/'
 SOLICITUDBYUSUARIO = 'solicitudbyusuario/'
@@ -8,7 +8,7 @@ SOLICITUDNOAPROBADAS = 'solicitudnoaprobadas/'
 SOLICITUDRECHAZAR = 'solicitudrechazar/'
 urlpatterns = [
     url(r'^'+SOLICITUD+'$', solicitudBajaLCView.as_view(), name="solicitud-lc"),
-    url(r'^'+SOLICITUD+SOLICITUD+'(?P<pk>\d+)$', solicitudBajaRUView.as_view(), name="solicitud-ru"),
+    url(r'^'+SOLICITUD+'(?P<pk>\d+)$', solicitudBajaRUView.as_view(), name="solicitud-ru"),
     url(r'^'+SOLICITUD+SOLICITUDNOAPROBADAS+'$', solicitudBajaLView.as_view(), name="solicitud-l"),
     url(r'^'+SOLICITUD+SOLICITUDAPROBAR+'(?P<pk>\d+)$', solicitudAprobar.as_view(), name="solicitud-u"),
     url(r'^'+SOLICITUD+SOLICITUDBYUSUARIO+'(?P<pk>\d+)$', solicitudByUsuarioL.as_view(), name="solicitudbyusuario-l"),
