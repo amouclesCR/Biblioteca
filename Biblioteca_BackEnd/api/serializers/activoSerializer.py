1682ac8d670630a1c474f363ab2ca7e4af0492e7
amoucles <alexcuatroojos@gmail.com>
2019-06-07 18:29:24 -0600

    documentacion del serializer de activo

--- Biblioteca_BackEnd/api/serializers/activoSerializer.py
+++ Biblioteca_BackEnd/api/serializers/activoSerializer.py
@@ -5,16 +5,19 @@ from .seccionSerializer import seccionSerializer
 from rest_framework.validators import UniqueValidator
 
 class activoSerializer(serializers.ModelSerializer):
-    #act_usuario_responsabe = usuarioSerializer(read_only=True, many=False)
+    
+    #   CAMPOS PARA ALMACENAR ENTIDAS RELACIONADAS CON LA ENTIDAD ACTIVO
     act_usuario = serializers.SerializerMethodField('get_usuario_responsable')
     act_seccion_modelo = serializers.SerializerMethodField('get_seccion')
     
+    #   OBTIENE EL USUARIO RESPONSABLE DEL ACTIVO
     def get_usuario_responsable(self, obj):
         return usuarioBySeccioSerializer(obj.act_usuario_responsabe).data
-
+    #   OBTIENE LA SECCION A LA CUAL PERTENECE EL ACTIVO
     def get_seccion(self, obj):
         return seccionSerializer(obj.act_seccion).data
     
+    #   CAMPOS A UTILIZAR EN EL JSON
     class Meta:
         model = AMBU_Activo
         fields = ('id', 
@@ -33,14 +36,16 @@ class activoSerializer(serializers.ModelSerializer):
         'act_seccion_modelo',
         'act_Fecha_Creacion'
         )
+
 class activoPUSerializer(serializers.ModelSerializer):
 
     #   VALIDACIONES DE CAMPOS
+    #   act_numero_activo -> VALIDA QUE EL CAMPO SEA ÚNICO
     act_numero_activo = serializers.CharField(max_length=100, 
         validators=[UniqueValidator(queryset=AMBU_Activo.objects.all(), 
         message="Ya existe un activo con ese número de activo")])
 
-    #   CAMPOS DEL JSON
+    #   CAMPOS A UTILIZAR EN EL JSON
     class Meta:
         model = AMBU_Activo
         fields = ('id', 
@@ -58,10 +63,15 @@ class activoPUSerializer(serializers.ModelSerializer):
         )
 
 class activoBySeccionSerializer(serializers.ModelSerializer):
+    
+    #   CAMPOS PARA ALMACENAR ENTIDAS RELACIONADAS CON LA ENTIDAD ACTIVO
     act_usuario = serializers.SerializerMethodField('get_usuario_responsable')
-
+    
+    #   OBTIENE EL USUARIO RESPONSABLE DEL ACTIVO
     def get_usuario_responsable(self, obj):
         return usuarioBySeccioSerializer(obj.act_usuario_responsabe).data
+
+    #   CAMPOS A UTLIZAR EN EL JSON    
     class Meta:
         model = AMBU_Activo
         fields = ('id', 
@@ -78,4 +88,3 @@ class activoBySeccionSerializer(serializers.ModelSerializer):
         'act_usuario',
         'act_Fecha_Creacion'
         )
-     #   depth = 1
