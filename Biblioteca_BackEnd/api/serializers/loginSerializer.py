6e886fae17f873c16b47f3c77627647e60bb82d6
amoucles <alexcuatroojos@gmail.com>
2019-06-07 18:33:03 -0600

    comentario en serializer de login

--- Biblioteca_BackEnd/api/serializers/loginSerializer.py
+++ Biblioteca_BackEnd/api/serializers/loginSerializer.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from Biblioteca_BackEnd.api.models import AMBU_Usuario
 
 class loginSerializer(serializers.ModelSerializer):
+
+    #   CAMPOS A UTILIZAR EN EL JSON
     class Meta:
         model = AMBU_Usuario
         fields = ('id', 'usu_identificacion', "usu_nombre", "usu_correo")
