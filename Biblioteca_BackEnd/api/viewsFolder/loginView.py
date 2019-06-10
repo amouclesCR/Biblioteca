06051cd94ba7f99d6405f3b1bca3840ad76d9bfd
amoucles <alexcuatroojos@gmail.com>
2019-06-09 18:10:07 -0600

    comentarios en loginView

--- Biblioteca_BackEnd/api/viewsFolder/loginView.py
+++ Biblioteca_BackEnd/api/viewsFolder/loginView.py
@@ -9,62 +9,40 @@ import json
 from django.contrib.auth import authenticate, logout, login
 from rest_framework.views import APIView
 
+#   REVISA SI EL USUARIO EXISTE PARA REALIZAR EL LOGIN DEL MISMO
 class loginCView (generics.CreateAPIView):
+
+    #   QUERY DE LA CONSULTA
     queryset = AMBU_CustomeUsuario.objects.all()
 
+    #   MAPEA LOS DATOS
     serializer_class = customSerializer
 
+    #   FUNCIÓN POST
     def post(self, request, *args, **kwargs):
 
-        email = kwargs.get('email')
-
+        #   OBTIENE LOS DATOS ENVIADOS EN EL POST
         body_unicode = request.body.decode('utf-8')
         body_data = json.loads(body_unicode)
+
+        #   QUERY PARA FILTRAR LOS DATOS
         query = AMBU_CustomeUsuario.objects.filter(
             username=body_data['username'])
-        test = False
+        
+        #   AUTENTICACIÓN DEL DEL USUARIO
         user = authenticate(
             username=body_data['username'], password=body_data['password'])
-        print(user)
+        
         if user:
             login(request, user)
             ser = customSerializer(user, many=False)
         else:
-            # print(query)
             ser = customSerializer(user, many=True)
         return Response(ser.data)
 
-
+#   REALIZA EL LOGOUT DEL USUARIO
 class logoutView (APIView):
 
      def post(self, request, format=None):
-        # simply delete the token to force a login
-        #request.user.auth_token.delete()
+       
         return Response(status=status.HTTP_200_OK)
-
-# from django.shortcuts import render
-# from rest_framework import generics, mixins
-# from Biblioteca_BackEnd.api.serializers.loginSerializer import loginSerializer
-# from Biblioteca_BackEnd.api.serializers.usuarioSerializer import usuarioListSerializer
-# from Biblioteca_BackEnd.api.models import AMBU_Usuario
-# from rest_framework.response import Response
-# from django.db.models import Q
-# import json
-
-
-# class loginCView (generics.CreateAPIView):
-#     queryset = AMBU_Usuario.objects.all()
-
-#     serializer_class = usuarioListSerializer
-
-#     def post(self, request, *args, **kwargs):
-#         body_unicode = request.body.decode('utf-8')
-#         body_data = json.loads(body_unicode)
-#         query = AMBU_Usuario.objects.filter(Q(usu_clave=body_data['usu_clave']) & Q(
-#             usu_identificacion=body_data['usu_identificacion']))
-#         if len(query) > 0:
-#             query = list(query)[0]
-#             ser = usuarioListSerializer(query, many=False)
-#         else:
-#             ser = usuarioListSerializer(query, many=True)
-#         return Response(ser.data)
