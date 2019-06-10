ea152a0fe1219cb8e5971a82251eb3a4adde3583
amoucles <alexcuatroojos@gmail.com>
2019-06-09 18:24:58 -0600

    comentarios en seccionView

--- Biblioteca_BackEnd/api/viewsFolder/seccionView.py
+++ Biblioteca_BackEnd/api/viewsFolder/seccionView.py
@@ -3,31 +3,42 @@ from rest_framework import generics, viewsets
 from rest_framework.response import Response
 from Biblioteca_BackEnd.api.serializers.seccionSerializer import  seccionSerializer, seccionUCSerializer
 from Biblioteca_BackEnd.api.models import AMBU_Seccion
-from rest_framework.permissions import IsAuthenticated
+
 # Create your views here.
+
+#   LISTA Y CREA LAS SECCIONES
 class SeccionLCView(generics.ListCreateAPIView):
-    #permission_classes = (IsAuthenticated,)
+    
+    #   QUERY DE LA CONSULTA
     queryset = AMBU_Seccion.objects.all()
     
+    #   MAPEA LOS DATOS
     serializer_class = seccionSerializer
 
     def get_serializer_class(self):
+
+        #   SI EL METODO ES POST USA seccionUCSerializer
         method = self.request.method
         if method == 'POST':
             return seccionUCSerializer
         else:
             return seccionSerializer
 
+#   OBTIENE O ACTUALIZA UNA SECCIO 
 class SeccionRUView(generics.RetrieveUpdateAPIView):
-   
-    lookup_field = 'pk' # PRIMARY KEY FROM URL
     
+    #   OBTIENE LA PK DE LA URL
+    lookup_field = 'pk' 
+    
+    #   CONSULTA
     queryset = AMBU_Seccion.objects.all()
-
+    
+    #   MAPEA LOS DATOS
     serializer_class = seccionSerializer
 
-   
     def get_serializer_class(self):
+
+        #   SI EL METODO ES PUT UTILIZA seccionUCSerializer
         method = self.request.method
         if method == 'PUT':
             return seccionUCSerializer
